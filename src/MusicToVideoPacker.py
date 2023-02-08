fb60551b8f2f374132f77ccc93244b3ccc9f433c
hackassen <hackassen@gmail.com>
2023-02-08 19:14:14 +0500

    assembling so so

--- src/MusicToVideoPacker.py
+++ src/MusicToVideoPacker.py
@@ -8,6 +8,7 @@ purposes...
 from moviepy.video.io import *
 from moviepy.audio.io import *
 from moviepy.audio import *
+from moviepy.editor import concatenate_videoclips
 import json
 
 import os, string, random
@@ -19,7 +20,7 @@ class MusicToVideoPacker:
     AUDIO_FILE_TYPES = [".mp3",] 
     DATA_DIR = "../data/"
     OUT_EXT = ".mp4"
-    OUT_PATH  = DATA_DIR+"PACKED"+OUT_EXT
+    #OUT_PATH  = DATA_DIR+"PACKED"+OUT_EXT
     META_FILE  = "META.txt"
     VIDEO_WIDTH = 240
     VIDEO_RESOLUTION = (240,240)
@@ -81,15 +82,29 @@ class MusicToVideoPacker:
             if audio_chunck_duration + song.duration > clip.duration:
                 
                 if audio_chunck_duration==0: # single song is longer than clip..
-                    # ! try to glue several vioes to the length of the song
-                    raise Exception("!!! ERROR the song is longer than the clip.")
-                
+
+                    #concetating clips to exeed the song duration                    
+                    clips_for_concat=[clip,]
+                    clips_for_concat_duration = clip.duration
+                    while clip_i<len(self.video_files):
+                        print(f" >> Concetating video {os.path.basename(self.video_files[clip_i])}")
+                        clip_i+=1
+                        newclip = VideoFileClip.VideoFileClip(self.video_files[clip_i], audio=False, target_resolution=self.VIDEO_RESOLUTION)
+                        clips_for_concat.append(newclip)
+                        clips_for_concat_duration+=newclip.duration
+                        if clips_for_concat_duration > song.duration: break
+                            
+                    #print(f" clips to concat {clips_for_concat}")
+                    clip=concatenate_videoclips(clips_for_concat)
+                    #print(f" conc done.")
+                                
                 #assembling songs and clip
                 assembledClip = self.AssembleClip(clip, songs)
                 audio_chunck_duration=song.duration
                 songs=[song,]
                 clip_i+=1
                 clip = VideoFileClip.VideoFileClip(self.video_files[clip_i], audio=False, target_resolution=self.VIDEO_RESOLUTION)            
+                continue
                 
                 #tp
                 #break
@@ -106,6 +121,7 @@ class MusicToVideoPacker:
         print("Packing done.")
         
     def AssembleClip(self, clip, songs:list):
+        
         audioTrack = AudioClip.CompositeAudioClip(songs)
         #print("!!!!!!!!!!!!!!!!!!")
         clip.audio = audioTrack    
@@ -129,7 +145,31 @@ class MusicToVideoPacker:
         print(f"Saving {flname}...")        
         clip.write_videofile(f"{self.DATA_DIR}{flname}")        
         clip.close()
-    
+
+    def Disassemble(self, assembly_path:str, out_path:str = "./"):
+        if not os.path.exists(assembly_path+"/"+self.META_FILE):
+            raise Exception("Invalid path to assembly. Please specify path containing 'META.txt'")
+        
+        with open(assembly_path+"/"+self.META_FILE, "r")as fl:
+            self.meta=json.load(fl)
+            
+        for clipnm in self.meta:
+            print(f"  {clipnm}  - disassembling...")
+            if not os.path.exists(assembly_path+"/"+clipnm):
+                print("f!! Error while disassembling: {clipnm} not found")
+            clip = VideoFileClip.VideoFileClip(assembly_path+"/"+clipnm, audio=True)
+            track = clip.audio
+            t=0
+            for song_meta in self.meta[clipnm]:
+                print(f"    {song_meta['name']}  - extracting...")
+                song = track.subclip(t, song_meta["duration"])
+                t+=song_meta["duration"]
+                song.write_audiofile(out_path+"/"+song_meta["name"])
+                song.close()
+                
+            clip.close()
+            
+        print("Disassembling done.")
                                 
             
 #from moviepy import VideoFileClip, concatenate_videoclips
@@ -140,7 +180,17 @@ class MusicToVideoPacker:
 if __name__=="__main__":
     video_dir = r"/home/hackassen/Downloads/movies/[Udemy] Python Programming Machine Learning, Deep Learning (05.2021)/"
     audio_dir =r"/home/hackassen/homehackassenDownloadsyndMusic/"
+
+    video_dir = r"/home/hackassen/works/MusicPacker/src/tp/"
+    audio_dir =r"/home/hackassen/works/MusicPacker/src/tpA/"
+
     packer = MusicToVideoPacker(video_dir, audio_dir)
     packer.list_videos()    
     packer.list_audios()    
     packer.Pack()
+    
+    
+    packed_path = r"/home/hackassen/works/MusicPacker/data/"
+    packed_path = r"/home/hackassen/tp/"
+    #packer.Disassemble(packed_path)
+    
